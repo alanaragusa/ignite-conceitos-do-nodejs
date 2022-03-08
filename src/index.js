9a5adbbf413e9370c636aef1535b3a489de5e73d
Alana Ragusa <alana.ragusarabello@gmail.com>
2022-03-08 17:41:32 -0300

    added app.patch and app.delete

--- src/index.js
+++ src/index.js
@@ -64,16 +64,16 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
   const { user } = request;
 
-  const todosOperation = {
+  const todo = {
     id: uuidv4(),
     title,
     done: false,
     deadline: new Date(deadline),
     created_at: new Date()
   }
-  user.todos.push(todosOperation);
+  user.todos.push(todo);
 
-  return response.status(201).send();
+  return response.status(201).json(todo);
 });
 
 // atualizar tarefa //
@@ -88,21 +88,40 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const updateToDo = user.todos.find(todo => todo.id === id);
 
   if(!updateToDo) {
-    return response.status(400).json({error:"Todo not found"})
+    return response.status(400).json({error:"Todo not found"});
   }
 
   updateToDo.title = title;
   updateToDo.deadline = new Date(deadline);
 
-  return response.status(201).send();
+  return response.status(201).json(updateToDo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const statusToDoChange = user.todos.find(todo => todo.id === id);
+  if (!statusToDoChange) {
+    return response.status(400).json({error:"Todo not found"});
+  }
+  statusToDoChange.done = true;
+
+  return response.json(statusToDoChange);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const deleteToDo = user.todos.findIndex(todo => todo.id === id);
+
+  if(deleteToDo === -1) {
+    return response.status(404).json({error:"Todo not found"});
+  }
+  user.todos.splice(deleteToDo, 1);
+  
+  return response.status(204).json(users);
 });
 
 module.exports = app;
